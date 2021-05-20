9d9b853de0acfa5fb21dcc12e7a65d6ff1fbdecd
JackChinnery43 <jack-chinnery@hotmail.com>
2021-05-20 13:53:20 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 mkdir -p ~/.streamlit/
 echo "\
+[theme]
+primaryColor="#328cc1"
+backgroundColor="#ffffff"
+secondaryBackgroundColor="#e5e5e5"
+textColor="#000000"
+font="sans serif"
 [server]\n\
 headless = true\n\
 port = $PORT\n\
