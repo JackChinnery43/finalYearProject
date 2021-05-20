895dc4a3d1f0db7f57c00759d17d1678dc021ee7
JackChinnery43 <jack-chinnery@hotmail.com>
2021-05-20 14:10:30 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,8 @@
 mkdir -p ~/.streamlit/
-
-echo "[theme]
-primaryColor="#328cc1"
-backgroundColor="#ffffff"
-secondaryBackgroundColor="#e5e5e5"
-textColor="#000000"
-font="sans serif"
-[server]
-headless = true
-port = $PORT
-enableCORS = false
+echo "
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
